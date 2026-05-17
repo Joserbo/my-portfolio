f70c9963e64a7f4e54c1bc48ee28eab2a26af5a3
Jose <josephrbo1997@gmail.com>
2026-05-17 01:43:59 -0600

    Initial React project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <div>
+      <h1>Jose Bustamante</h1>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h2>Welcome to my portfolio</h2>
+      <p>I build AI-powered web applications!</p>
+    </section>
+  )
+}
+
+export default Hero
